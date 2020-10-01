d5aefa2dbcf819c06531f44a6712c326ff933706
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-09-30 19:12:29 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+/* Aqui vamos a comprobar si el formulario fue enviado */
+$errores = '';
+$enviado = '';
+/* isset detemina si una variable esta efinida  y no es null */
+if (isset($_POST['submit'])){
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
+
+    /*  empty nos ayuda a saber si hay contenido en una variable
+        y con el signo ! le decimos si no hay o ( SI ES DIFERENTE )
+
+        trim: quita los espacios que fueron guardos en una vaiable al inico o final
+        filter_var()    Nos permite sanear, osea eliminar caracteres que no nos sirven HTML
+    */
+    if (!empty($nombre)){
+        $nombre = trim($nombre);
+        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+    }  else {
+        /*  el simbolo .= le estamos diciento que la variable $errores  
+            nos concatene el texto siguiente con el salto de linea
+        */
+        $errores .= 'Porfavor ingresa un nombre <br />';
+    }
+
+    /* ------------------------------------------------------------- */
+    /* Vaidacion de correo */
+    if (!empty($correo)) {
+        $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+
+        /*  Esta funcion con este parametro nos regresa verdadero o falso */
+        /*  En !filter_var estamos pregundo si encuentras un texto que
+            no es un correo entonces, queremos que hagas lo siguiente
+        */
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+            $errores .= 'Por favor ingresa un correo valido <br />';
+        } 
+        
+    } else {
+            $errores .= 'Por favor ingresa un correo <br />';
+    }
+    /* ------------------------------------------------------------- */
+    /* Validacion de mensaje */
+    if (!empty($mensaje)) {
+        $mensaje = htmlspecialchars($mensaje);
+        $mensaje = trim($mensaje);
+        $mensaje = stripslashes($mensaje);
+    } else {
+        $errores .= 'Por favor ingresa el mensaje <br />'; 
+    }
+    /* ------------------------------------------------------------- */
+    /* Vamos a preguntar si hay errores */
+
+    if (!$errores) {
+        $enviar_a = '1510.lalo.51@gmail.com';
+        $asunto = 'Correo enviado desde MiPagina.com';
+        $mensaje_preprarado = "De: $nombre \n";
+        $mensaje_preprarado .= "Correo: $correo \n";
+        $mensaje_preprarado .= "Mensaje: " . $mensaje;
+
+        mail($enviar_a, $asunto, $mensaje_preprarado);
+        $enviado = 'true';
+    }
+
+
+}
+
+require 'index.view.php';
+?>
---
+++ inedx.view.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="fonts.css">
+  <link rel="stylesheet" href="estilos.css">
+  <link rel="stylesheet" href="colors.css">
+  <title>Formulario Contacto</title>
+</head>
+
+<body>
+
+  <!-- contenedor para mostrar mensajes de error y enviado exitosamente -->
+  <div class="container pt-3">
+    <div class="row d-flex justify-content-center">
+      <div class="col-8">
+        <!-- Si no esta vacia la variable errores, significa que si hay un error que queremos mostrar-->
+        <?php if (!empty($errores)) :  ?>
+          <div class="alert alert-danger small">
+            <strong>!Aviso</strong><br>
+            <?php echo $errores; ?>
+          </div>
+        <?php elseif ($enviado) : ?>
+          <div class="alert alert-success small">
+            <p>Enviado correctamente</p>
+          </div>
+          <br>
+        <?php endif  ?>
+      </div>
+    </div>
+  </div>
+
+  <!-- Contenedor principal del formulario -->
+  <div class="container">
+    <div class="row justify-content-center">
+      <!-- Formulario -->
+      <div class="col-md-4 estilos-form">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+          <h2 class="text-center pt-2">Formulario de Contacto</h2>
+          <hr>
+          <div class="form-group">
+            <label for="nombre">Nombre:</label>
+            <input type="text" class="form-sm-control d-block w-100" id="nombre" placeholder="Nombre" name="nombre" value="<?php if (!$enviado && isset($nombre)) echo $nombre ?>">
+          </div>
+
+          <div class="form-group">
+            <label for="correo">Correo:</label>
+            <input type="email" class="form-sm-control d-block w-100" id="correo" placeholder="correo@gmail.com" name="correo" value="<?php if (!$enviado && isset($correo)) echo $correo ?>">
+          </div>
+
+          <div class="form-group">
+            <label for="mensaje">Mensaje:</label>
+            <textarea type="textarea" class="form-sm-control d-block w-100" id="mensaje" rows="5" placeholder="Mensaje" name="mensaje"><?php if (!$enviado && isset($mensaje)) echo $mensaje ?></textarea>
+          </div>
+
+          <div class="form-group">
+            <input type="submit" name="submit" class="form-sm-control d-block w-100 btn btn-success" value="Enviar mensaje" value=""></input>
+          </div>
+        </form>
+      </div>
+      <!-- Imagen -->
+      <div class="col-md-4 d-flex flex-column align-items-end estilos-form-img">
+        <img src="contacto.svg" class="mt-auto img-fluid ">
+      </div>
+    </div>
+  </div>
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
